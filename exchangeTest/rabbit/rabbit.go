626102d8dd63c12b6f9519372cb2fa15da4e915f
vladimir <vladimir1012kov@gmail.com>
2023-11-01 15:50:41 +0300

    #2 добавлены функции для взаимодействия с RabbitMQ

--- exchangeTest/rabbit/rabbit.go
+++ exchangeTest/rabbit/rabbit.go
@@ -1,10 +1,15 @@
 package rabbit
 
 import (
+	"context"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 
 	log "github.com/sirupsen/logrus"
 )
+
+// TODO: Работа консюмера (подписка на очередь)
+// TODO: Работа паблишера (отправка сообщения в очередь)
 
 // Функция создания подключения к RabbitMQ
 func createConnRabbit(URL string) (*amqp.Connection, error) {
@@ -39,4 +44,71 @@ func declaringEchange(rabbitChan *amqp.Channel, nameExchange string, typeExchang
 		log.Error(err.Error())
 	}
 	return err
+}
+
+// Функция декларирования очереди
+func declaringQueue(rabbitChan *amqp.Channel, nameQueue string) (amqp.Queue, error) {
+	queue, err := rabbitChan.QueueDeclare(
+		nameQueue,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Errorf("Ошибка создания очереди: %s", err.Error())
+	}
+	return queue, err
+}
+
+// Функция binding очереди к Exchange
+func bindingQueue(rabbitChan *amqp.Channel, nameQueue, nameExchange, routingKey string) error {
+	err := rabbitChan.QueueBind(
+		nameQueue,
+		routingKey,
+		nameExchange,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Errorf("Ошибка binding очереди %s, к exchange %s : %s", nameQueue, nameExchange, err.Error())
+	}
+	return err
+}
+
+// Функция отправки сообщения в Exchange
+func sendingMess(chanRabbit *amqp.Channel, ctx context.Context, exchangeName, routingKey, mess string) error {
+	err := chanRabbit.PublishWithContext(
+		ctx,
+		exchangeName,
+		routingKey,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(mess),
+		},
+	)
+
+	if err != nil {
+		log.Errorf("Ошибка отправки сообщения: %s", err.Error())
+	}
+	log.Debugf("Сообщение отправлено: %s", mess)
+	return err
+}
+
+// Функция закрытия канала и коннекта RabbitMQ
+func exitAndClose(chanRb *amqp.Channel, connRb *amqp.Connection, whoIsIt string) {
+	errChan := chanRb.Close()
+	if errChan != nil {
+		log.Errorf("Ошибка закрытия канала RabbitMQ: %s", whoIsIt)
+	}
+
+	errConn := connRb.Close()
+	if errConn != nil {
+		log.Errorf("Ошибка закрытия коннекта RabbitMQ: %s", whoIsIt)
+	}
+
+	log.Warningf("Закрыты канал и коннект к RabbitMQ: %s", whoIsIt)
 }

5e055b5333c7b5568a62a0d0f7f2a62f777bbd5b
vladimir <vladimir1012kov@gmail.com>
2023-11-02 17:25:22 +0300

    #4 работа над модулем маршрутизации

---
+++ exchangeTest/router/constants.go
@@ -0,0 +1,5 @@
+package router
+
+const (
+	EventType = 1
+)
---
+++ exchangeTest/router/models.go
@@ -0,0 +1,41 @@
+package router
+
+import (
+	"time"
+)
+
+// Канал для отправки событий
+type EventChan chan Event
+
+// Событие, которое отправляет publisher
+type Event struct {
+	id          int
+	publisher   string
+	typeEvent   string
+	timePublish time.Time
+	message     string
+}
+
+// Канал для отправки подписчиков
+type SubscriberChan chan SubscriberMess
+
+// Сообщение содержащее информацию о подписчике
+type SubscriberMess struct {
+	name   string
+	types  []string
+	evenCh EventChan
+}
+
+// Структура содержащая каналы маршрутизаторов
+type routersChans struct {
+	typeCh    chan Event
+	publishCh chan Event
+	allCh     chan Event
+}
+
+// Структура которую возвращает инициализатор маршрутизатора (любого типа)
+type eventRoutKIT struct {
+	eventCh  EventChan
+	subscrCh SubscriberChan
+	cancel   func()
+}
